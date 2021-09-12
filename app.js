6385552f0b36dd480926cb591e2030ddb64cc727
parthmodi444 <88581955+parthmodi444@users.noreply.github.com>
2021-09-12 15:19:10 +0530

    color chnge

---
+++ app.js
@@ -0,0 +1,28 @@
+var txtInput=document.querySelector("#txt-input");
+var outputDiv=document.querySelector("#output");
+var btnTranslate=document.querySelector('#btn-translate');
+btnTranslate.addEventListener("click",clickHandler);
+// var serverUrl="https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+
+var serverUrl="https://api.funtranslations.com/translate/";
+function getTranslationUrl(text)
+{
+    return serverUrl +"?"+"text="+text;
+}
+function errorHandler(error)
+{
+    console.log("error occured",error)
+}
+function clickHandler()
+{
+ var inputText=txtInput.value;
+ fetch(getTranslationUrl(inputText))
+ .then(response =>response.json())
+ .then(json =>
+    {
+        var translatedText=json.contents.translated;
+        outputDiv.innerText=translatedText;
+    }
+    )
+ .catch(errorHandler)
+};
